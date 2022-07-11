6c88f48f2f4f9157e18d103552b8d00e28dce3e5
dex1e <dex1e5@yandex.ru>
2022-07-12 00:59:10 +0600

    feat: added normalize, reset, create userModal

---
+++ src/Components/styles.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const Modal = styled.div`
+width: 100vw;
+height: 100vw;
+background-color:rgba(0,0,0,0.4);
+position:fixed;
+top:0;
+left:0;
+display: flex;
+/* align-items: center; */
+justify-content:center;  
+`
+
+export const ModalContent = styled.div`
+padding: 20px;
+border-radius: 12px;
+background-color:white;
+width:400px;
+height:200px;
+`
+
---
+++ src/Components/userModal.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { ModalContent, Modal } from './styles';
+
+
+
+const UserModal = () => {
+    return (
+        <div>
+            <Modal>
+                <ModalContent></ModalContent>
+            </Modal>
+        </div>
+    )
+}
+export default UserModal
