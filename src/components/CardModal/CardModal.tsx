81065dcab6bf943ee24b0c013d18838a6a8289a5
dex1e <dex1e5@yandex.ru>
2022-07-21 17:13:02 +0600

    feat: css in CardModal

--- src/components/CardModal/CardModal.tsx
+++ src/components/CardModal/CardModal.tsx
@@ -6,7 +6,6 @@ import { ICard } from "types";
 
 interface CardModalProps {
   // username: string;
-  // columnTitle: string;
   // isCardVisible: boolean;
   // setCardVisible: (isCardVisible: boolean) => void;
   // textArea: string;
@@ -15,18 +14,31 @@ interface CardModalProps {
 export const CardModal: FC<CardModalProps> = ({
   card,
   // setCardVisible,
-  // columnTitle,
   // username,
   // textArea,
 }) => {
   return (
     <Modal>
-      <ModalWindow>
-        <div>{card.title}</div>
-      </ModalWindow>
+      <ModalWrapper>
+        <ModalWindow>
+          <Header>
+            <TitleInput></TitleInput>
+            <CloseModal />
+          </Header>
+        </ModalWindow>
+      </ModalWrapper>
     </Modal>
   );
 };
+
+const ModalWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: var(--shadow);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const ModalWindow = styled.div`
   /* display: block;
@@ -41,18 +53,57 @@ const ModalWindow = styled.div`
 
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 600px;
-  height: 700px;
+  max-width: 750px;
+  width: 100%;
+  height: 90%;
+  max-height: 800px;
   background-color: var(--white);
   border-radius: 7px;
   border: 1px solid var(--gray);
-  transform: scale()
   filter: drop-shadow(0px 0px 10px var(--shadow));
-  gap: 15px;
+  /* gap: 15px; */
 
-  @media (max-width: 425px) {
+  /* @media (max-width: 425px) {
     margin: 5%;
+  } */
+`;
+
+const Header = styled.header`
+  width: 100%;
+  min-height: 30px;
+  display: flex;
+
+  /* padding: 20px; */
+`;
+
+const TitleInput = styled.input`
+  font-size: 20px;
+  width: 95%;
+  min-height: 30px;
+  font-weight: 600;
+  padding: 7px;
+  border-radius: 7px;
+  cursor: text;
+  margin: 10px;
+
+  &:focus {
+    /* box-shadow: 0 0 0 2px var(--lightskyblue); */
+    background-color: var(--lightgray);
+    border: 1px solid var(--black);
   }
+
+  &:hover {
+    background-color: var(--lightgray);
+    border: 1px solid var(--black);
+  }
+`;
+
+const CloseModal = styled.div`
+  width: 20px;
+  height: 20px;
+  background-color: black;
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: 5px;
 `;
