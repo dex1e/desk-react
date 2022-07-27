44b3f2ae77c5bc63b2d1f37be4981b7726348787
dex1e <dex1e5@yandex.ru>
2022-07-27 21:59:54 +0600

    fix: delete initialState

--- src/store/features/comments/commentsSlice.ts
+++ src/store/features/comments/commentsSlice.ts
@@ -3,21 +3,26 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 import { IComment } from "types";
 import { v4 as uuidv4 } from "uuid";
 
-import { initialState } from "./initialState";
-import { IAddComment, IRenameComment } from "./types";
+import {
+  IAddCommentPayload,
+  ICommentInitialState,
+  IRenameCommentPayload,
+} from "./types";
+
+const initialState: ICommentInitialState = { comments: {} };
 
 export const commentsSlice = createSlice({
   name: "cards",
   initialState,
 
   reducers: {
-    renameComment: (state, action: PayloadAction<IRenameComment>) => {
+    renameComment: (state, action: PayloadAction<IRenameCommentPayload>) => {
       const { commentId, newCommentText } = action.payload;
 
       state.comments[commentId].text = newCommentText;
     },
 
-    addComment: (state, action: PayloadAction<IAddComment>) => {
+    addComment: (state, action: PayloadAction<IAddCommentPayload>) => {
       const commentId = uuidv4();
 
       const { commentText, cardId, user } = action.payload;
